bb2f5f5e420dfba4710810ebed957e440140013a
www220@tom.com <www220@tom.com>
2015-09-21 23:08:38 +0800

    整理libc函数

--- components/libc/vslib/syscalls.c
+++ components/libc/vslib/syscalls.c
@@ -4,9 +4,14 @@
 
 #ifdef RT_USING_DFS
 #include <dfs_posix.h>
+
+#ifdef RT_USING_DFS_DEVFS
+#include <devfs.h>
 #endif
 
-void libc_system_init()
+#endif
+
+void libc_system_init(void)
 {
 #ifdef RT_USING_DFS
     int fd;
@@ -16,16 +21,20 @@ void libc_system_init()
 #error Please enable devfs by defining RT_USING_DFS_DEVFS in rtconfig.h
 #endif
 
-    /* init console device */
-    extern void rt_console_init(const char* device_name);
     console_dev = rt_console_get_device();
     if (console_dev)
+    {
+        /* initialize console device */
         rt_console_init(console_dev->parent.name);
 
-    /* open console as stdin/stdout/stderr */
-    fd = open("/dev/console", O_RDONLY, 0);	/* for stdin */
-    fd = open("/dev/console", O_WRONLY, 0);	/* for stdout */
-    fd = open("/dev/console", O_WRONLY, 0);	/* for stderr */
+        /* open console as stdin/stdout/stderr */
+        fd = open("/dev/console", O_RDONLY, 0); /* for stdin */
+        fd = open("/dev/console", O_WRONLY, 0); /* for stdout */
+        fd = open("/dev/console", O_WRONLY, 0); /* for stderr */
+
+        /* skip warning */
+        fd = fd;
+    }
 #endif
 }
 
